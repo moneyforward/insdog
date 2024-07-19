e6af7e77bc50f975f290b67d17629380d176cbf4
Ukai Hiroshi <dakusui@gmail.com>
2024-07-19 18:50:58 +0900

    Introduce 'precheck' feature and extract version declarations

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,106 @@
 #!/bin/bash -eu
 
+# Component versions to install
+COMPONENT_VERSIONS="$(cat <<'EOF'
+JDK:    21.0.2-open
+MAVEN:  3.9.6
+GOLANG: 1.21.6
+EOF
+)"
+
 function sed() {
   /usr/bin/sed "${@}"
 }
 
+# A function to ensure macOS default grep is used.
 function grep() {
   /usr/bin/grep "${@}"
 }
 
 function projectbrew() {
   "${brewdir}/bin/brew" "${@}"
+}
+
+function version_for() {
+  local _component_name="${1}"
+  echo "${COMPONENT_VERSIONS}" | grep "${_component_name}" | cut -f 2 -d ':' | sed -E 's/^ +//g'
+}
+
+function jdk_version() {
+  version_for "JDK"
+}
+function maven_version() {
+  version_for "MAVEN"
+}
+function golang_version() {
+  version_for "GOLANG"
+}
+
+# Reads standard streams (stdout and stderr) and assign the content of them to variables specified by the first and
+# second parameters respectively.
+# Note that this function doesn't declare variables specified by the parameters.
+# They need to be defined in the caller side if necessary.
+# Also note that the behavior is undefined, when a variable name which starts with `__read_std_` is specified.
+# 1:  A variable name to which stdout from the parameter 3- will be assigned.
+# 2:  A variable name to which stderr from the parameter 3- will be assigned.
+# 3-: A command line to be executed by this function.
+function read_std() {
+  local __read_std_stdout_varname __read_std_stderr_varname
+  local __read_std_tmpfile_stderr __read_std_stdout_content __read_std_stderr_content __read_std_exit_code=0
+  __read_std_stdout_varname="$(printf '%q' "${1}")"
+  __read_std_stderr_varname="$(printf '%q' "${2}")"
+  shift; shift
+  __read_std_tmpfile_stderr="$(mktemp)"
+  __read_std_stdout_content="$("${@}" 2> "${__read_std_tmpfile_stderr}")" || {
+    __read_std_exit_code=$?
+  }
+  __read_std_stdout_content="$(printf '%q' "${__read_std_stdout_content}")"
+  __read_std_stderr_content="$(cat "${__read_std_tmpfile_stderr}")"
+  __read_std_stderr_content="$(printf '%q' "${__read_std_stderr_content}")"
+
+  eval "${__read_std_stdout_varname}=${__read_std_stdout_content}"
+  eval "${__read_std_stderr_varname}=${__read_std_stderr_content}"
+
+  rm -f "${__read_std_tmpfile_stderr}"
+  return "${__read_std_exit_code}"
+}
+
+function __bootstrap__perform_checks() {
+  local _installation_reportdir="${1}" _session_name="${2}"
+  local _failed=0
+  local _stdout _stderr _session_dir="${1}/${_session_name}"
+  shift
+  shift
+  mkdir -p "${_session_dir}"
+  for _i in "${@}"; do
+    mkdir -p "${_session_dir}/${_i}"
+    read_std _stdout _stderr "${_i}" || {
+      echo "${_stdout}" > "${_session_dir}/${_i}/stdout"
+      echo "${_stderr}" > "${_session_dir}/${_i}/stderr"
+      echo "FAIL: <${_i}>" >&2
+      _failed=$((_failed + 1))
+      continue
+    }
+    echo "${_stdout}" > "${_session_dir}/${_i}/stdout"
+    echo "${_stderr}" > "${_session_dir}/${_i}/stderr"
+    echo "pass: <${_i}>" >&2
+  done
+  echo "----"
+  echo "FAILED CHECKS: ${_failed}" >&2
+  [[ 0 == "${_failed}" ]] || return 1
+}
+
+function __bootstrap__checkenv() {
+  local _installation_reportdir="${1}"
+  local _checks=()
+  function is_orbctl_installed() {
+    which orbctl
+  }
+  _checks+=("is_orbctl_installed")
+  __bootstrap__perform_checks \
+    "${_installation_reportdir}" \
+    "pre-check" \
+    "${_checks[@]}"
 }
 
 function install_project_homebrew() {
@@ -26,7 +117,7 @@ function install_project_homebrew() {
 }
 
 
-function bootstrap() {
+function bootstrap_homebrew() {
   local _homebrew_dir="${1}"
   install_project_homebrew "${_homebrew_dir}" | progress "BOOTSTRAP" 2>&1 /dev/null
 }
@@ -83,7 +174,7 @@ function message() {
 
 function main() {
   local _projectdir _project_dependencies_dir _project_brewdir  _project_rcdir _caveats_file
-  local _project_godir _goenv_file _go_version
+  local _project_godir _goenv_file
   local _project_sdkman_dir
   _projectdir="$(dirname "${1}")"
   _projectdir="$(realpath "${_projectdir}")"
@@ -95,14 +186,19 @@ function main() {
   _caveats_file="${_project_dependencies_dir}/rc/caveats.rc"
 
   _goenv_file="${_project_dependencies_dir}/rc/goenv.rc"
-  _go_version="1.21.6"
   _project_godir="${_project_dependencies_dir}/go"
 
   _project_sdkman_dir="${_project_dependencies_dir}/sdkman"
   shift
 
+  local _precheck_reportdir=".dependencies/bootstrap"
+  mkdir -p "${_precheck_reportdir}"
+
+  # Performs precheck
+  __bootstrap__checkenv "${_precheck_reportdir}"
+
   mkdir -p "${_project_rcdir}"
-  bootstrap "${_project_brewdir}"
+  bootstrap_homebrew "${_project_brewdir}"
   reset_caveats_rc "${_caveats_file}"
 
   export HOMEBREW_NO_AUTO_UPDATE=0
@@ -119,20 +215,20 @@ function main() {
   # golang
   mkdir -p "${_project_godir}/env"
   install_brew_package "${_project_brewdir}" goenv   > /dev/null
-  compose_goenv_rc "${_project_godir}" "${_go_version}" > "${_goenv_file}"
+  compose_goenv_rc "${_project_godir}" "$(golang_version)" > "${_goenv_file}"
   # shellcheck disable=SC1090
   source "${_goenv_file}"
-  "${_project_brewdir}/bin/goenv" install -q "${_go_version}" 2>&1 | progress "goenv:${_go_version}"
+  "${_project_brewdir}/bin/goenv" install -q "$(golang_version)" 2>&1 | progress "goenv:${_go_version}"
   "${_project_godir}/env/versions/${_go_version}/bin/go" install github.com/Songmu/make2help/cmd/make2help@latest 2>&1 | progress "go:make2help"
 
   # sdkman & Java
   export HOME="${_project_rcdir}" # To avoid .bashrc / .bash_profile / .zsh_profile being updated
   export SDKMAN_DIR="${_project_sdkman_dir}"
   # install sdkman
-  curl -s "https://get.sdkman.io" | /bin/bash 2>&1 | progress "sdkman"
+  curl -s "https://get.sdkman.io"    | /bin/bash 2>&1 | progress "sdkman"
 
-  sdk_install java 21.0.2-open | progress "sdkman:openjdk"
-  sdk_install maven 3.9.6      | progress "sdkman:maven"
+  sdk_install java "$(jdk_version)"  | progress "sdkman:openjdk"
+  sdk_install maven "$(maven_version)" | progress "sdkman:maven"
 }
 
 projectdir="$(dirname "${BASH_SOURCE[0]}")"
@@ -140,4 +236,3 @@ projectdir="$(realpath "${projectdir}")"
 brewdir="${projectdir}/.dependencies/homebrew"
 
 main "${0}" "$@"
-
