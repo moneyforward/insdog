b385d7e626ce7f20ba1aaca6f1d1baa7e79e8347
Ukai Hiroshi <dakusui@gmail.com>
2024-07-24 10:50:58 +0900

    Move browserContextFrom method to accessing model class

--- src/main/java/jp/co/moneyforward/autotest/ca_web/core/ExecutionProfile.java
+++ src/main/java/jp/co/moneyforward/autotest/ca_web/core/ExecutionProfile.java
@@ -1,47 +1,8 @@
 package jp.co.moneyforward.autotest.ca_web.core;
 
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserContext;
-
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
-
-import static jp.co.moneyforward.autotest.framework.utils.InternalUtils.date;
-import static jp.co.moneyforward.autotest.framework.utils.InternalUtils.today;
-
 public interface ExecutionProfile {
   static ExecutionProfile create() {
     return new ExecutionProfileImpl();
-  }
-  
-  /**
-   * Creates a browser context object from the browser `b`.
-   *
-   * @param b                A browser.
-   * @param executionProfile An execution profile.
-   * @return A browser context object.
-   */
-  static BrowserContext browserContextFrom(Browser b, ExecutionProfile executionProfile) {
-    BrowserContext c;
-    if (today().after(date(executionProfile.plannedDateForSettingUpSelfhostedGitHubActions()))) {
-      c = b.newContext(new Browser.NewContextOptions()
-                           .setLocale(executionProfile.locale()));
-    } else {
-      // Base64 encode the credentials
-      String username = "money-book";
-      String password = "asdfasdf";
-      String basicAuth = Base64.getEncoder().encodeToString((username + ":" + password).getBytes());
-      
-      // Set up the context with the BASIC Auth headers
-      Browser.NewContextOptions contextOptions = new Browser.NewContextOptions();
-      Map<String, String> headers = new HashMap<>();
-      headers.put("Authorization", "Basic " + basicAuth);
-      contextOptions.setExtraHTTPHeaders(headers)
-                    .setLocale(executionProfile.locale());
-      c = b.newContext(contextOptions);
-    }
-    return c;
   }
   
   String plannedDateForSettingUpSelfhostedGitHubActions();
