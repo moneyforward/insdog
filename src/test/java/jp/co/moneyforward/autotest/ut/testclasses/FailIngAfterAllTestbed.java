cc3559d6a4b29ece82e80cbb6a4da9da588198e6
Ukai Hiroshi <dakusui@gmail.com>
2024-08-02 19:55:45 +0900

    Make branch created by 'assert' statement ignored by JaCoCo

---
+++ src/test/java/jp/co/moneyforward/autotest/ut/testclasses/FailIngAfterAllTestbed.java
@@ -0,0 +1,19 @@
+package jp.co.moneyforward.autotest.ut.testclasses;
+
+import com.github.dakusui.actionunit.exceptions.ActionException;
+import jp.co.moneyforward.autotest.framework.action.Scene;
+import jp.co.moneyforward.autotest.framework.annotations.AutotestExecution;
+import jp.co.moneyforward.autotest.framework.annotations.Named;
+
+@AutotestExecution(defaultExecution = @AutotestExecution.Spec(
+    value = {"login", "connect", "disconnect", "logout"},
+    afterAll = {"fail"}))
+public class FailIngAfterAllTestbed extends TestbedBase {
+  @Named
+  public static Scene fail() {
+    return new Scene.Builder("fail").add("out",
+                                         (value, executionEnvironment) -> {
+                                           throw new ActionException("Intentional Exception!");
+                                         }).build();
+  }
+}
