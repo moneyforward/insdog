321817e46f6c2d0ba7eb34708222874b0ad3dd2b
Ukai Hiroshi <dakusui@gmail.com>
2024-08-17 06:46:46 +0900

    Add tests for internal utility methods

--- src/test/java/jp/co/moneyforward/autotest/ut/cli/CliUtilsTest.java
+++ src/test/java/jp/co/moneyforward/autotest/ut/cli/CliUtilsTest.java
@@ -4,18 +4,19 @@ import jp.co.moneyforward.autotest.framework.cli.CliUtils;
 import jp.co.moneyforward.autotest.ut.cli.testpackage.tags.Hello1TaggedClass;
 import jp.co.moneyforward.autotest.ut.cli.testpackage.tags.Hello2TaggedClass;
 import jp.co.moneyforward.autotest.ut.cli.testpackage.tags.NoTaggedClass;
+import jp.co.moneyforward.autotest.ututils.TestBase;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static com.github.valid8j.fluent.Expectations.assertStatement;
-import static com.github.valid8j.fluent.Expectations.value;
+import static com.github.valid8j.fluent.Expectations.*;
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class CliUtilsTest {
+class CliUtilsTest extends TestBase {
   @Test
   void whenListTags_thenTaggedClassesReturned() {
     List<String> tags = CliUtils.listTags(
@@ -116,4 +117,39 @@ class CliUtilsTest {
     
   }
   
+  @Test
+  void whenComposeSceneDescriptorPropertyValue() {
+    var composedPropertyValue = CliUtils.composeSceneDescriptorPropertyValue(new String[]{"beforeAll=beforeAllStep", "beforeEach=beforeEachStep", "value=test1,test2", "afterEach=afterEachStep", "afterAll=afterAllStep"});
+    System.out.println(composedPropertyValue);
+    List<String> list = asList(composedPropertyValue.substring("inline:".length())
+                                                    .split(";"));
+    assertAll(value(composedPropertyValue).toBe().startingWith("inline:"),
+              value(list).size().toBe().equalTo(5),
+              value(list).toBe().containing("beforeAll=beforeAllStep"),
+              value(list).toBe().containing("beforeEach=beforeEachStep"),
+              value(list).toBe().containing("value=test1,test2"),
+              value(list).toBe().containing("afterEach=afterEachStep"),
+              value(list).toBe().containing("afterAll=afterAllStep"));
+  }
+  
+  @Test
+  void givenOperatorSkippedQueryForClassname_whenParseQuery_thenUnsupportedOperationException() {
+    var predicate = CliUtils.parseQuery("classname:XYZ");
+    System.out.println(predicate);
+    assertStatement(value(predicate).stringify().toBe().equalTo("simpleName isEqualTo[XYZ]"));
+  }
+  
+  @Test
+  void givenOperatorSkippedQueryForTagName_whenParseQuery_thenUnsupportedOperationException() {
+    var predicate = CliUtils.parseQuery("tag:XYZ");
+    System.out.println(predicate);
+    assertStatement(value(predicate).stringify().toBe().equalTo("hasTagMatching[valueEqualTo[XYZ]]"));
+  }
+  
+  @Test
+  void givenSubstringOperatorForTagName_whenParseQuery_thenUnsupportedOperationException() {
+    var predicate = CliUtils.parseQuery("tag:%XYZ");
+    System.out.println(predicate);
+    assertStatement(value(predicate).stringify().toBe().equalTo("hasTagMatching[valueContaining[XYZ]]"));
+  }
 }
