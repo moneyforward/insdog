f7d0b39359b6c9e8bf0cb78b70356c3e89ae26b6
Ukai Hiroshi <dakusui@gmail.com>
2024-10-02 12:49:36 +0900

    Remove unused imports

--- src/test/java/jp/co/moneyforward/autotest/ut/caweb/accessmodels/CawebAccessingModelFactoryTest.java
+++ src/test/java/jp/co/moneyforward/autotest/ut/caweb/accessmodels/CawebAccessingModelFactoryTest.java
@@ -1,11 +1,9 @@
 package jp.co.moneyforward.autotest.ut.caweb.accessmodels;
 
 import jp.co.moneyforward.autotest.ca_web.core.CawebExecutionProfile;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.stream.Stream;
 
