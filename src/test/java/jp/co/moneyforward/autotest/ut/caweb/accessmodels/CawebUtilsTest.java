fa481de272ccb01130a71b3242db54ce072b878d
sukezan <suke.3gs@icloud.com>
2024-10-11 17:29:28 +0900

    [fix]: update Unit test for CawebUtils

--- src/test/java/jp/co/moneyforward/autotest/ut/caweb/accessmodels/CawebUtilsTest.java
+++ src/test/java/jp/co/moneyforward/autotest/ut/caweb/accessmodels/CawebUtilsTest.java
@@ -2,6 +2,7 @@ package jp.co.moneyforward.autotest.ut.caweb.accessmodels;
 
 import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.AriaRole;
 import jp.co.moneyforward.autotest.actions.web.PageAct;
 import jp.co.moneyforward.autotest.ca_web.accessmodels.CawebUtils;
 import jp.co.moneyforward.autotest.framework.core.ExecutionEnvironment;
@@ -9,6 +10,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.xml.sax.helpers.LocatorImpl;
 
+import javax.management.relation.Role;
+
+import static com.microsoft.playwright.options.AriaRole.LINK;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -26,6 +30,30 @@ class CawebUtilsTest {
     pageAct.perform(page, env);
     
     verify(locatorToBeClicked).click();
+  }
+  
+  @Test
+  void whenNavigateToNewTabUnderSidebarItemAndAct_thenPageActPerformed() {
+    ExecutionEnvironment env = Mockito.mock(ExecutionEnvironment.class);
+    Locator menuItemLocator = Mockito.mock(Locator.class);
+    Locator menuSubItemLocator = Mockito.mock(Locator.class);
+    Page page = Mockito.mock(Page.class);
+    Page newPage = Mockito.mock(Page.class);
+    PageAct pageAct = Mockito.mock(PageAct.class);
+    String menuItem = "Settings";
+    String menuSubItem = "Profile";
+    
+    when(page.getByText(menuItem)).thenReturn(menuItemLocator);
+    when(page.waitForPopup(any())).thenReturn(newPage);
+    when(page.getByRole(LINK, new Page.GetByRoleOptions().setName(menuSubItem))).thenReturn(menuSubItemLocator);
+    
+    PageAct pageAct1 = CawebUtils.navigateToNewTabUnderSidebarItemAndAct(menuItem, menuSubItem, pageAct);
+    pageAct1.perform(page, env);
+    
+    verify(menuItemLocator).click();
+    verify(page).waitForPopup(any());
+//    verify(menuSubItemLocator).click();
+    verify(pageAct).perform(newPage, env);
   }
   
   @Test
@@ -60,29 +88,74 @@ class CawebUtilsTest {
     verify(locatorToBeWaitedOn).waitFor(any());
   }
   
+//  @Test
+//  void whenElementIsEqualTo_thenElementIsEqualTo() {
+//    ExecutionEnvironment env = Mockito.mock(ExecutionEnvironment.class);
+//    Page page = Mockito.mock(Page.class);
+//    Locator locator = Mockito.mock(Locator.class);
+//    when(page.locator(any(String.class))).thenReturn(locator);
+//
+//    PageAct pageAct = CawebUtils.elementIsEqualTo("DUMMY > SELECTOR > STRING>", "Hello, world!");
+//    pageAct.perform(page, env);
+//
+//    verify(locator).textContent().equals("Hello, world!");
+//  }
+  
   @Test
-  void whenAssertAlertSuccessIsDisplayed_thenAlertSuccessIsDisplayed() {
+  void whenExportDataSpecifiedFormat_thenPageActPerformedInNewTab() {
     ExecutionEnvironment env = Mockito.mock(ExecutionEnvironment.class);
     Page page = Mockito.mock(Page.class);
-    Locator locatorToBeVisible = (Locator) Mockito.mock(LocatorImpl.class);
-    when(page.locator("#alert-success > p")).thenReturn(locatorToBeVisible);
+    Page newPage = Mockito.mock(Page.class);
+    PageAct pageAct = Mockito.mock(PageAct.class);
+    Locator locator = Mockito.mock(Locator.class);
+    Locator fileFormatLocator = Mockito.mock(Locator.class);
+    Role role = Mockito.mock(Role.class);
     
-    PageAct pageAct = CawebUtils.assertAlertSuccessIsDisplayed();
-    pageAct.perform(page, env);
+    String locatorExportButton = "#export-button";
+    String dataFormat = "PDF";
     
-    verify(locatorToBeVisible).isVisible();
+    when(page.locator(locatorExportButton)).thenReturn(locator);
+    when(page.waitForPopup(any())).thenReturn(newPage);
+    when(page.getByRole(LINK, new Page.GetByRoleOptions().setName(dataFormat))).thenReturn(fileFormatLocator);
+    
+    PageAct pageAct1 = CawebUtils.exportDataSpecifiedFormat(locatorExportButton, dataFormat, pageAct);
+    pageAct1.perform(page, env);
+    
+    verify(locator).click();
+    verify(page).waitForPopup(any());
+//    verify(fileFormatLocator).click();
+    verify(pageAct).perform(newPage, env);
   }
   
   @Test
-  void whenElementIsEqualTo_thenElementIsEqualTo() {
+  void whenClickButtonToDisplayModalAndEnterDepartmentNameAndRegister_thenModalDisplayedAndDepartmentRegistered() {
     ExecutionEnvironment env = Mockito.mock(ExecutionEnvironment.class);
     Page page = Mockito.mock(Page.class);
-    Locator locator = Mockito.mock(Locator.class);
-    when(page.locator(any(String.class))).thenReturn(locator);
+    Locator buttonLocator = Mockito.mock(Locator.class);
+    Locator modalLocator = Mockito.mock(Locator.class);
+    Locator deptNameLocator = Mockito.mock(Locator.class);
+    Locator addDeptButtonLocator = Mockito.mock(Locator.class);
+    Locator closeModalButtonLocator = Mockito.mock(Locator.class);
     
-    PageAct pageAct = CawebUtils.elementIsEqualTo("DUMMY > SELECTOR > STRING>", "Hello, world!");
+    String locatorButton = "#add-dept-button";
+    String value = "New Department";
+    
+    when(page.locator(locatorButton)).thenReturn(buttonLocator);
+    when(buttonLocator.first()).thenReturn(buttonLocator);
+    when(page.locator("#js-add-dept-modal")).thenReturn(modalLocator);
+    when(modalLocator.isVisible()).thenReturn(true);
+    when(modalLocator.locator("#dept_name")).thenReturn(deptNameLocator);
+    when(modalLocator.locator("#js-btn-add-dept")).thenReturn(addDeptButtonLocator);
+    when(modalLocator.locator("#btn-modal-close > img")).thenReturn(closeModalButtonLocator);
+    
+    PageAct pageAct = CawebUtils.clickButtonToDisplayModalAndEnterDepartmentNameAndRegister(locatorButton, value);
     pageAct.perform(page, env);
     
-    verify(locator).textContent().equals("Hello, world!");
+    verify(buttonLocator).click();
+    verify(page).waitForSelector("#js-add-dept-modal");
+    verify(modalLocator).isVisible();
+    verify(deptNameLocator).fill(value);
+    verify(addDeptButtonLocator).click();
+    verify(closeModalButtonLocator).click();
   }
 }
